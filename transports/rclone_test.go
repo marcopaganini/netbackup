060dd55e037a16389bbb6f7d18e3c733246d041c
Marco Paganini <paganini@paganini.net>
2015-12-05 21:15:50 -0800

    Added unit tests for transports (rclone for now).

---
+++ transports/rclone_test.go
@@ -0,0 +1,187 @@
+// This file is part of netbackup (http://github.com/marcopaganini/netbackup)
+// See instructions in the README.md file that accompanies this program.
+// (C) 2015 by Marco Paganini <paganini AT paganini DOT net>
+
+package transports
+
+import (
+	"github.com/marcopaganini/netbackup/config"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+	"testing"
+)
+
+// Common rclone tests: Initialize an rclone instance with the config passed
+// and test fail if the generated command doesn't match the expected command
+// line regexp. The value of dryRun is passed to the initializon of rclone. If
+// must_error is true, the call to NewRcloneTransport *must* fail, or the test
+// will fail.
+func rcloneTest(t *testing.T, cfg *config.Config, expect string, dryRun bool, mustError bool) {
+	fakeRunner := NewFakeRunner()
+	rclone, err := NewRcloneTransport(cfg, fakeRunner, 0, dryRun)
+	if err != nil {
+		if mustError {
+			return
+		}
+		t.Fatalf("rclone.NewRcloneTransport failed: %v", err)
+	}
+	if mustError {
+		t.Fatalf("rclone.NewRcloneTransport should return have failed, but got nil error")
+	}
+	if err := rclone.Run(); err != nil {
+		t.Fatalf("rclone.Run failed: %v", err)
+	}
+	matched, err := regexp.MatchString(expect, fakeRunner.Cmd())
+	if err != nil {
+		t.Fatalf("error during regexp match: %v", err)
+	}
+	if !matched {
+		t.Fatalf("name should match %s; is %s", expect, fakeRunner.Cmd())
+	}
+}
+
+// Dry run: No command should be executed
+func TestDryRun(t *testing.T) {
+	cfg := &config.Config{
+		Name:      "fake",
+		SourceDir: "/tmp/a",
+		DestDir:   "/tmp/b",
+		Transport: "rclone",
+		Logfile:   "/dev/null",
+	}
+	rcloneTest(t, cfg, "", true, false)
+}
+
+// Same machine (local copy)
+func TestSameMachine(t *testing.T) {
+	cfg := &config.Config{
+		Name:      "fake",
+		SourceDir: "/tmp/a",
+		DestDir:   "/tmp/b",
+		Transport: "rclone",
+		Logfile:   "/dev/null",
+	}
+	rcloneTest(t, cfg, "rclone sync -v /tmp/a /tmp/b", false, false)
+}
+
+// Local source, remote destination
+func TestLocalSourceRemoteDest(t *testing.T) {
+	cfg := &config.Config{
+		Name:      "fake",
+		SourceDir: "/tmp/a",
+		DestDir:   "/tmp/b",
+		DestHost:  "desthost",
+		Transport: "rclone",
+		Logfile:   "/dev/null",
+	}
+	rcloneTest(t, cfg, "rclone sync -v /tmp/a desthost:/tmp/b", false, false)
+}
+
+// Remote source, local destination (unusual)
+func TestRemoteSourceLocalDest(t *testing.T) {
+	cfg := &config.Config{
+		Name:       "fake",
+		SourceHost: "srchost",
+		SourceDir:  "/tmp/a",
+		DestDir:    "/tmp/b",
+		Transport:  "rclone",
+		Logfile:    "/dev/null",
+	}
+	rcloneTest(t, cfg, "rclone sync -v srchost:/tmp/a /tmp/b", false, false)
+}
+
+// Remote source, Remote destination (server side copy)
+func TestRemoteSourceRemoteDest(t *testing.T) {
+	cfg := &config.Config{
+		Name:       "fake",
+		SourceHost: "srchost",
+		SourceDir:  "/tmp/a",
+		DestHost:   "desthost",
+		DestDir:    "/tmp/b",
+		Transport:  "rclone",
+		Logfile:    "/dev/null",
+	}
+	rcloneTest(t, cfg, "rclone sync -v srchost:/tmp/a desthost:/tmp/b", false, false)
+}
+
+// Exclude list only
+func TestExcludeListOnly(t *testing.T) {
+	cfg := &config.Config{
+		Name:        "fake",
+		SourceDir:   "/tmp/a",
+		DestDir:     "/tmp/b",
+		ExcludeList: []string{"x/foo", "x/bar"},
+		Transport:   "rclone",
+		Logfile:     "/dev/null",
+	}
+	rcloneTest(t, cfg, "rclone sync -v --exclude=[^ ]+ /tmp/a /tmp/b", false, false)
+}
+
+// Include list only
+func TestIncludeListOnly(t *testing.T) {
+	cfg := &config.Config{
+		Name:        "fake",
+		SourceDir:   "/tmp/a",
+		DestDir:     "/tmp/b",
+		IncludeList: []string{"x/foo", "x/bar"},
+		Transport:   "rclone",
+		Logfile:     "/dev/null",
+	}
+	rcloneTest(t, cfg, "rclone sync -v --include=[^ ]+ /tmp/a /tmp/b", false, false)
+}
+
+// Include & Exclude lists
+func TestIncludeAndExclude(t *testing.T) {
+	cfg := &config.Config{
+		Name:        "fake",
+		SourceDir:   "/tmp/a",
+		DestDir:     "/tmp/b",
+		ExcludeList: []string{"x/foo", "x/bar"},
+		IncludeList: []string{"x/foo", "x/bar"},
+		Transport:   "rclone",
+		Logfile:     "/dev/null",
+	}
+	rcloneTest(t, cfg, "rclone sync -v --exclude=[^ ]+ --include=[^ ]+ /tmp/a /tmp/b", false, false)
+}
+
+// Test that an empty source dir results in an error
+func TestEmptySourceDir(t *testing.T) {
+	cfg := &config.Config{
+		Name:      "fake",
+		DestDir:   "/tmp/b",
+		Transport: "rclone",
+		Logfile:   "/dev/null",
+	}
+	rcloneTest(t, cfg, "", false, true)
+}
+
+// Test that an empty destination dir results in an error
+func TestEmptyDestDir(t *testing.T) {
+	cfg := &config.Config{
+		Name:      "fake",
+		SourceDir: "/tmp/a",
+		Transport: "rclone",
+		Logfile:   "/dev/null",
+	}
+	rcloneTest(t, cfg, "", false, true)
+}
+
+// Test writeList
+func TestWriteList(t *testing.T) {
+	items := []string{"aa", "aa/01", "aa/02", "bb"}
+	fname, err := writeList("fakename", items)
+	if err != nil {
+		t.Fatalf("writeList failed: %v", err)
+	}
+	contents, err := ioutil.ReadFile(fname)
+	os.Remove(fname)
+	if err != nil {
+		t.Fatalf("Unable to read list file %q: %v", fname, err)
+	}
+	expected := strings.Join(items, "\n") + "\n"
+	if string(contents) != expected {
+		t.Fatalf("generated list file contents should match\n[%s]\n\nbut is\n\n[%s]", expected, string(contents))
+	}
+}
---
+++ transports/transports_test.go
@@ -0,0 +1,37 @@
+// This file is part of netbackup (http://github.com/marcopaganini/netbackup)
+// See instructions in the README.md file that accompanies this program.
+// (C) 2015 by Marco Paganini <paganini AT paganini DOT net>
+
+package transports
+
+import (
+	"github.com/marcopaganini/netbackup/runner"
+	"strings"
+)
+
+// This file contains all the shared infrastructure required for the transports
+// tests. Individual transports tests go in their respective *_test.go files.
+
+// Fake runner implementation
+type FakeRunner struct {
+	cmd string
+}
+
+func NewFakeRunner() *FakeRunner {
+	return &FakeRunner{}
+}
+
+func (f *FakeRunner) SetStdout(runner.CallbackFunc) {
+}
+
+func (f *FakeRunner) SetStderr(runner.CallbackFunc) {
+}
+
+func (f *FakeRunner) Cmd() string {
+	return f.cmd
+}
+
+func (f *FakeRunner) Exec(a []string) error {
+	f.cmd = strings.Join(a, " ")
+	return nil
+}
