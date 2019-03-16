08405a4f0de4819381941778783214e8671d9499
Marco Paganini <paganini@paganini.net>
2019-03-16 00:05:00 -0700

    Rclone tests are now table driven.

--- transports/rclone_test.go
+++ transports/rclone_test.go
@@ -7,169 +7,167 @@ package transports
 import (
 	"github.com/marcopaganini/logger"
 	"github.com/marcopaganini/netbackup/config"
-	"regexp"
 	"testing"
 )
 
-// Common rclone tests: Initialize an rclone instance with the config passed
-// and test fail if the generated command doesn't match the expected command
-// line regexp. The value of dryRun is passed to the initializon of rclone. If
-// must_error is true, the call to NewRcloneTransport *must* fail, or the test
-// will fail.
-func rcloneTest(t *testing.T, cfg *config.Config, expect string, dryRun bool, mustError bool) {
-	fakeExecute := NewFakeExecute()
+func TestRclone(t *testing.T) {
+	casetests := []struct {
+		name       string
+		sourceDir  string
+		sourceHost string
+		destDir    string
+		destHost   string
+		transport  string
+		logfile    string
+		expectCmds []string
+		include    []string
+		exclude    []string
+		dryRun     bool
+		wantError  bool
+	}{
+		// Dry run: No command should be executed
+		{
+			name:      "fake",
+			sourceDir: "/tmp/a",
+			destDir:   "/tmp/b",
+			transport: "rclone",
+			logfile:   "/dev/null",
+			dryRun:    true,
+		},
+		// Same machine (local copy)
+		{
+			name:       "fake",
+			sourceDir:  "/tmp/a",
+			destDir:    "/tmp/b",
+			transport:  "rclone",
+			logfile:    "/dev/null",
+			expectCmds: []string{"rclone sync -v /tmp/a /tmp/b"},
+		},
+		// Local source, remote destination
+		{
+			name:       "fake",
+			sourceDir:  "/tmp/a",
+			destDir:    "/tmp/b",
+			destHost:   "desthost",
+			transport:  "rclone",
+			logfile:    "/dev/null",
+			expectCmds: []string{"rclone sync -v /tmp/a desthost:/tmp/b"},
+		},
+		// Remote source, local destination (unusual)
+		{
+			name:       "fake",
+			sourceHost: "srchost",
+			sourceDir:  "/tmp/a",
+			destDir:    "/tmp/b",
+			transport:  "rclone",
+			logfile:    "/dev/null",
+			expectCmds: []string{"rclone sync -v srchost:/tmp/a /tmp/b"},
+		},
+		// Remote source, Remote destination (server side copy)
+		{
+			name:       "fake",
+			sourceHost: "srchost",
+			sourceDir:  "/tmp/a",
+			destHost:   "desthost",
+			destDir:    "/tmp/b",
+			transport:  "rclone",
+			logfile:    "/dev/null",
+			expectCmds: []string{"rclone sync -v srchost:/tmp/a desthost:/tmp/b"},
+		},
+		// exclude: list only
+		{
+			name:       "fake",
+			sourceDir:  "/tmp/a",
+			destDir:    "/tmp/b",
+			exclude:    []string{"x/foo", "x/bar"},
+			transport:  "rclone",
+			logfile:    "/dev/null",
+			expectCmds: []string{"rclone sync -v --exclude-from=[^ ]+ /tmp/a /tmp/b"},
+		},
+		// include: list only
+		{
+			name:       "fake",
+			sourceDir:  "/tmp/a",
+			destDir:    "/tmp/b",
+			include:    []string{"x/foo", "x/bar"},
+			transport:  "rclone",
+			logfile:    "/dev/null",
+			expectCmds: []string{"rclone sync -v --include-from=[^ ]+ /tmp/a /tmp/b"},
+		},
+		// include: & exclude: lists
+		{
+			name:       "fake",
+			sourceDir:  "/tmp/a",
+			destDir:    "/tmp/b",
+			exclude:    []string{"x/foo", "x/bar"},
+			include:    []string{"x/foo", "x/bar"},
+			transport:  "rclone",
+			logfile:    "/dev/null",
+			expectCmds: []string{"rclone sync -v --exclude-from=[^ ]+ --include-from=[^ ]+ /tmp/a /tmp/b"},
+		},
+		// Test that an empty source dir results in an error
+		{
+			name:      "fake",
+			destDir:   "/tmp/b",
+			transport: "rclone",
+			logfile:   "/dev/null",
+			wantError: true,
+		},
+		// Test that an empty destination dir results in an error
+		{
+			name:      "fake",
+			sourceDir: "/tmp/a",
+			transport: "rclone",
+			logfile:   "/dev/null",
+			wantError: true,
+		},
+	}
 
-	log := logger.New("")
+	for _, tt := range casetests {
+		fakeExecute := NewFakeExecute()
 
-	// Create a new rclone object with our fakeExecute and a sinking outLogWriter.
-	rclone, err := NewRcloneTransport(cfg, fakeExecute, log, dryRun)
-	if err != nil {
-		if mustError {
-			return
+		log := logger.New("")
+
+		cfg := &config.Config{
+			Name:       tt.name,
+			SourceDir:  tt.sourceDir,
+			SourceHost: tt.sourceHost,
+			DestDir:    tt.destDir,
+			DestHost:   tt.destHost,
+			Transport:  tt.transport,
+			Logfile:    tt.logfile,
+			Include:    tt.include,
+			Exclude:    tt.exclude,
 		}
-		t.Fatalf("rclone.NewRcloneTransport failed: %v", err)
-	}
-	if mustError {
-		t.Fatalf("rclone.NewRcloneTransport should return have failed, but got nil error")
-	}
-	if err := rclone.Run(); err != nil {
-		t.Fatalf("rclone.Run failed: %v", err)
-	}
-	cmds := ""
-	if len(fakeExecute.Cmds()) != 0 {
-		cmds = fakeExecute.Cmds()[0]
-	}
-	matched, err := regexp.MatchString(expect, cmds)
-	if err != nil {
-		t.Fatalf("error during regexp match: %v", err)
-	}
-	if !matched {
-		t.Fatalf("name should match %s; is %s", expect, cmds)
-	}
-}
 
-// Dry run: No command should be executed
-func TestRcloneDryRun(t *testing.T) {
-	cfg := &config.Config{
-		Name:      "fake",
-		SourceDir: "/tmp/a",
-		DestDir:   "/tmp/b",
-		Transport: "rclone",
-		Logfile:   "/dev/null",
-	}
-	rcloneTest(t, cfg, "", true, false)
-}
+		// Create a new transport object with our fakeExecute and a sinking outLogWriter.
+		rclone, err := NewRcloneTransport(cfg, fakeExecute, log, tt.dryRun)
+		if tt.wantError && err != nil {
+			continue
+		}
+		if err != nil {
+			t.Fatalf("NewRcloneTransport failed: %v", err)
+		}
 
-// Same machine (local copy)
-func TestRcloneSameMachine(t *testing.T) {
-	cfg := &config.Config{
-		Name:      "fake",
-		SourceDir: "/tmp/a",
-		DestDir:   "/tmp/b",
-		Transport: "rclone",
-		Logfile:   "/dev/null",
+		if err := rclone.Run(); err != nil {
+			t.Fatalf("rclone.Run failed: %v", err)
+		}
+		if !tt.wantError {
+			if err != nil {
+				t.Fatalf("Got error %q want no error", err)
+			}
+			match, err := reMatch(tt.expectCmds, fakeExecute.Cmds())
+			if err != nil {
+				t.Fatalf("Error on regexp match: %v", err)
+			}
+			if !match {
+				t.Fatalf("command diff: Got %v, want %v", fakeExecute.Cmds(), tt.expectCmds)
+			}
+			continue
+		}
+		// Here, we want to see an error.
+		if err == nil {
+			t.Errorf("Got no error, want error")
+		}
 	}
-	rcloneTest(t, cfg, "rclone sync -v /tmp/a /tmp/b", false, false)
-}
-
-// Local source, remote destination
-func TestRcloneLocalSourceRemoteDest(t *testing.T) {
-	cfg := &config.Config{
-		Name:      "fake",
-		SourceDir: "/tmp/a",
-		DestDir:   "/tmp/b",
-		DestHost:  "desthost",
-		Transport: "rclone",
-		Logfile:   "/dev/null",
-	}
-	rcloneTest(t, cfg, "rclone sync -v /tmp/a desthost:/tmp/b", false, false)
-}
-
-// Remote source, local destination (unusual)
-func TestRcloneRemoteSourceLocalDest(t *testing.T) {
-	cfg := &config.Config{
-		Name:       "fake",
-		SourceHost: "srchost",
-		SourceDir:  "/tmp/a",
-		DestDir:    "/tmp/b",
-		Transport:  "rclone",
-		Logfile:    "/dev/null",
-	}
-	rcloneTest(t, cfg, "rclone sync -v srchost:/tmp/a /tmp/b", false, false)
-}
-
-// Remote source, Remote destination (server side copy)
-func TestRcloneRemoteSourceRemoteDest(t *testing.T) {
-	cfg := &config.Config{
-		Name:       "fake",
-		SourceHost: "srchost",
-		SourceDir:  "/tmp/a",
-		DestHost:   "desthost",
-		DestDir:    "/tmp/b",
-		Transport:  "rclone",
-		Logfile:    "/dev/null",
-	}
-	rcloneTest(t, cfg, "rclone sync -v srchost:/tmp/a desthost:/tmp/b", false, false)
-}
-
-// Exclude list only
-func TestRcloneExcludeListOnly(t *testing.T) {
-	cfg := &config.Config{
-		Name:      "fake",
-		SourceDir: "/tmp/a",
-		DestDir:   "/tmp/b",
-		Exclude:   []string{"x/foo", "x/bar"},
-		Transport: "rclone",
-		Logfile:   "/dev/null",
-	}
-	rcloneTest(t, cfg, "rclone sync -v --exclude-from=[^ ]+ /tmp/a /tmp/b", false, false)
-}
-
-// Include list only
-func TestRcloneIncludeListOnly(t *testing.T) {
-	cfg := &config.Config{
-		Name:      "fake",
-		SourceDir: "/tmp/a",
-		DestDir:   "/tmp/b",
-		Include:   []string{"x/foo", "x/bar"},
-		Transport: "rclone",
-		Logfile:   "/dev/null",
-	}
-	rcloneTest(t, cfg, "rclone sync -v --include-from=[^ ]+ /tmp/a /tmp/b", false, false)
-}
-
-// Include & Exclude lists
-func TestRcloneIncludeAndExclude(t *testing.T) {
-	cfg := &config.Config{
-		Name:      "fake",
-		SourceDir: "/tmp/a",
-		DestDir:   "/tmp/b",
-		Exclude:   []string{"x/foo", "x/bar"},
-		Include:   []string{"x/foo", "x/bar"},
-		Transport: "rclone",
-		Logfile:   "/dev/null",
-	}
-	rcloneTest(t, cfg, "rclone sync -v --exclude-from=[^ ]+ --include-from=[^ ]+ /tmp/a /tmp/b", false, false)
-}
-
-// Test that an empty source dir results in an error
-func TestRcloneEmptySourceDir(t *testing.T) {
-	cfg := &config.Config{
-		Name:      "fake",
-		DestDir:   "/tmp/b",
-		Transport: "rclone",
-		Logfile:   "/dev/null",
-	}
-	rcloneTest(t, cfg, "", false, true)
-}
-
-// Test that an empty destination dir results in an error
-func TestRcloneEmptyDestDir(t *testing.T) {
-	cfg := &config.Config{
-		Name:      "fake",
-		SourceDir: "/tmp/a",
-		Transport: "rclone",
-		Logfile:   "/dev/null",
-	}
-	rcloneTest(t, cfg, "", false, true)
 }
